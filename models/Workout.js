a66475a22601c6815efcbaedcb69e5626c93b94f
Grant Smith <grantnsmith@gmail.com>
2020-06-30 11:26:08 -0700

    Created Workout Schema.

---
+++ models/Workout.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    excercises: [
+        {
+    type: String,
+    name: String,
+    distance: Number,
+    duration: Number,
+    weight: Number,
+    sets: Number,
+    reps: Number,
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
