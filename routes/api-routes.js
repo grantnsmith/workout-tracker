ad30456c16c1cdddb9b19de4cf745fd8d078ebf7
Grant Smith <grantnsmith@gmail.com>
2020-06-30 18:11:14 -0700

    Added GET api-routes.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,43 @@
 const db = require("../models");
+const mongoose = require("mongoose");
+
 
 module.exports = function(app) {
+
+// Find all workouts for stats page
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+    .then (workoutData => {
+        res.json(workoutData);
+    })
+    .catch(err => {
+        res.json(err);
+    })
+});
+
+// Get current workout
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+    .sort({ day: -1 })
+    .limit(1)
+    .then(workoutData => {
+        res.json(workoutData);
+    })
+    .catch( err => {
+        res.json(err);
+    })
+  });
+
+// Create new workout
+app.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body, (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    });
+  });
+
 
 };
